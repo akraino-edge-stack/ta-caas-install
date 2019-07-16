fee64acb8539ffc51197c3d33f6caa67731d827f
Krisztian Lengyel <krisztian.lengyel@nokia.com>
2019-07-16 15:06:31 +0200

    Upgrade DANM to version 4.0
    This upgrade introduces a new component named `danm_webhook`.
    Previously statically templated DANM CRDs now also come form the repo
    in build time.
    
    Change-Id: I923b5e7aab7841006567681b071ddb0d292209cc
    Signed-off-by: Krisztian Lengyel <krisztian.lengyel@nokia.com>

--- ansible/filter_plugins/config_helpers.py
+++ ansible/filter_plugins/config_helpers.py
@@ -14,20 +14,55 @@
 # limitations under the License.
 
 from ansible.errors import AnsibleError
+import itertools
 
 
 class FilterModule(object):
     def filters(self):
         return {
+            'get_provider_network_interfaces': get_provider_network_interfaces,
+            'get_provider_networks': get_provider_networks,
+            'get_sriov_provider_network_interfaces': get_sriov_provider_network_interfaces,
+            'get_sriov_provider_networks': get_sriov_provider_networks,
+            'filter_network_profiles_by_type': filter_network_profiles_by_type,
+            'filter_provider_networks_by_type': filter_provider_networks_by_type,
             'get_kube_options': get_kube_options,
             'get_mapped_key': get_mapped_key,
         }
 
 
-def get_kube_options(options):
-    if not isinstance(options, dict):
-        raise AnsibleError("Invalid type {}. Options must be dictionary!".format(type(options)))
+def get_provider_network_interfaces(provider_network_interfaces, provider_network):
+    return [interface
+            for interface, data in provider_network_interfaces.iteritems()
+            if provider_network in data.get('provider_networks', [])]
 
+
+def get_provider_networks(network_interfaces):
+    return list(itertools.chain.from_iterable(
+        [interface.get('provider_networks', [])
+         for interface in network_interfaces.itervalues()]))
+
+
+def get_sriov_provider_network_interfaces(profile, provider_network):
+    return profile.get('sriov_provider_networks', {}).get(provider_network, {}).get('interfaces', [])
+
+
+def get_sriov_provider_networks(profile):
+    return profile.get('sriov_provider_networks', {}).keys()
+
+
+def filter_network_profiles_by_type(profiles, key, type):
+    return {name: profile for name, profile in profiles.iteritems()
+            if key in profile and filter((lambda x: x.get('type', "") == type), profile[key].itervalues())}
+
+
+def filter_provider_networks_by_type(profile, type):
+    return {name: network for name, network in profile.iteritems()
+            if network.get('type', "") == type}
+
+
+def get_kube_options(options):
+    _validate_dict(options)
     option_template = "{}={}"
     formated_options = [option_template.format(option, str(value))
                         for option, value in options.iteritems()]
@@ -44,4 +79,4 @@ def get_mapped_key(mapping, search_key, key_name):
 
 def _validate_dict(value):
     if not isinstance(value, dict):
-        raise AnsibleError("Invalid type {}. Options must be dictionary!".format(type(options)))
+        raise AnsibleError("Invalid type {}. Options must be dictionary!".format(type(value)))
